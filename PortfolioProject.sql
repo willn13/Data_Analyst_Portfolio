a43ec607deccd011090a4ba78db12e80d4c728f2
willn13 <100555472+willn13@users.noreply.github.com>
2022-05-14 16:02:31 +0900

    Add files via upload

---
+++ PortfolioProject.sql
@@ -0,0 +1,117 @@
+﻿SELECT *
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+order by 3,4
+
+SELECT location,date,total_cases,new_cases,total_deaths, population
+FROM PortfolioProject..CovidDeaths
+ORDER BY 1,2
+
+
+-- Covidで亡くなる確率
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE location = 'Japan'
+ORDER BY 1,2
+
+-- Covidに感染した人口の割合
+SELECT location, date, total_cases, population, (total_cases/population)*100 as CovidPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE location = 'Japan'
+ORDER BY 1,2
+
+-- Highest infection rate compared to population
+SELECT location, MAX(total_cases) as HighestInfectionCount, population, Max((total_cases/population))*100 as InfectedPopulation
+FROM PortfolioProject..CovidDeaths
+GROUP BY location,population
+ORDER BY InfectedPopulation desc
+
+--人口にあたり死亡者数
+SELECT location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+GROUP BY location
+ORDER BY TotalDeathCount desc
+
+--大陸別に見てみましょう。
+SELECT location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+WHERE continent is null AND location NOT IN ('World',
+					'Upper middle income','High income',
+					'Lower middle income', 'Low income', 'European Union', 
+					'International')
+GROUP BY location
+ORDER BY TotalDeathCount desc
+
+
+
+-- 世界の亡くなる確率
+
+SELECT date, SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+Group By date
+ORDER BY 1,2
+
+-- 今の世界の亡くなる確率
+
+SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+ORDER BY 1,2
+
+-- 総人口と予防接種の比較
+
+WITH PopvsVac (Continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
+as
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations as BIGINT)) OVER (Partition by dea.location 
+Order by dea.location, dea.date) as RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent is not null
+--ORDER BY 2,3
+)
+SELECT*, (RollingPeopleVaccinated/Population)*100
+FROM PopvsVac
+
+
+テーブル
+DROP Table if exists #PercentPopulationVaccinated
+CREATE Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+INSERT INTO #PercentPopulationVaccinated
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations as BIGINT)) OVER (Partition by dea.location 
+Order by dea.location, dea.date) as RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent is not null
+--ORDER BY 2,3
+
+SELECT*, (RollingPeopleVaccinated/Population)*100
+FROM #PercentPopulationVaccinated
+
+-- 後で可視化するためのデータを保存するViewの作成
+
+Create View PercentPopulationVaccinated as
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CAST(vac.new_vaccinations as BIGINT)) OVER (Partition by dea.location 
+Order by dea.location, dea.date) as RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent is not null
